83a0ae45c5d30e61ae2316f43cf644d269023f00
UcanUup <liuyangyang@liuyangyangdeMacBook-Pro.local>
2017-02-08 20:09:38 +0800

    Initial Commit

---
+++ RandomApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RandomApp
+//
+//  Created by Young on 2017/2/8.
+//  Copyright © 2017年 Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RandomApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RandomApp
+//
+//  Created by Young on 2017/2/8.
+//  Copyright © 2017年 Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
